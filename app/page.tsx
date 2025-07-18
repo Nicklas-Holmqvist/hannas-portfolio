14478f6201c7f52488343f62f61f7038fa043b4b
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2025-07-18 16:06:02 +0200

    feat: added connection to DatoCMS

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,37 @@
+import { datoRequest } from '@/lib/datocms';
 import ThemeToggle from './theme/ThemeToggle';
+import { notFound } from 'next/navigation';
 
-export default function Home() {
+type Landingpage = {
+  allLandingpages: {
+    id: number;
+    title: string;
+  }[];
+};
+
+async function getLandingpage() {
+  const query = `query work {
+    allLandingpages {
+    id
+    title
+  }
+  }`;
+
+  const response = (await datoRequest({
+    query: query,
+  })) as Landingpage | null;
+
+  if (response === null) return notFound();
+  return response;
+}
+async function Home() {
+  const data: Landingpage = await getLandingpage();
+  console.log(data);
   return (
     <div className="">
+      <h1>{data.allLandingpages[0].title}</h1>
       <ThemeToggle />
     </div>
   );
 }
+export default Home;
