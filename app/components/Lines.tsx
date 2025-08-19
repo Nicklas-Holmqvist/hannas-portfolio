628d75aa84055b8cc991046c766488f436503d56
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2025-08-19 10:37:55 +0200

    feat: added lines between section

---
+++ app/components/Lines.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Lines() {
+  return (
+    <div className="flex w-full justify-center py-10">
+      <img src="/plant_line.svg" height={200} width={200} alt="" />
+    </div>
+  );
+}
+
+export default Lines;
