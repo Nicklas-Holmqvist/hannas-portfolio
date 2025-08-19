b685ebf5a1eefdd18ba3c7e71fbc603f4def2c08
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2025-08-19 08:40:08 +0200

    style: fixed space between sections and bodies

--- app/components/InfoSection.tsx
+++ app/components/InfoSection.tsx
@@ -14,7 +14,7 @@ type Data = {
 
 function InfoSection({ data }: InfoSectionProps) {
   const secondaryContent = data.secondTitle ? (
-    <div className="mt-16">
+    <div className="pt-10">
       <h3 className="text-center">{data.secondTitle}</h3>
       <p className="text-center max-w-2xl">
         <ReactMarkdown>{data.secondInformation}</ReactMarkdown>
